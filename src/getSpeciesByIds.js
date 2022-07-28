c94504a5644a761efad73ac4ca3722bd2b9c5651
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2022-07-28 15:50:48 -0300

    R1: função getSpeciesByIds implementada

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-  // iniciando o projeto
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  let animal = [];
+  species.forEach((specie) => {
+    const { id } = specie;
+    for (let element = 0; element <= ids.length; element += 1) {
+      if (id === ids[element]) {
+        animal = [...animal, specie];
+      }
+    }
+  });
+  return animal;
 }
 
 module.exports = getSpeciesByIds;
