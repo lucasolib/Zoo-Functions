9236c01820c139ac2047f6cc90b88fc75b90956c
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2022-07-28 17:50:41 -0300

    R2: função getAnimalsOderThan implementada

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  let olderThan;
+  species.forEach((specie) => {
+    const { name } = specie;
+    if (name === animal) {
+      const { residents } = specie;
+      let animalAges = [];
+      for (let index = 0; index < residents.length; index += 1) {
+        const animalAge = residents[index].age;
+        animalAges = [...animalAges, animalAge];
+      }
+      olderThan = animalAges.every((animalAge) => age <= animalAge);
+      return olderThan;
+    }
+  });
+  return olderThan;
 }
 
 module.exports = getAnimalsOlderThan;
