9b2216b04323b774db2806af2751f014fec888fe
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2022-07-29 15:11:08 -0300

    R8: função calculateEntry implementada

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,35 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  entrants.forEach((person) => {
+    const { age } = person;
+    if (age <= 17) {
+      child += 1;
+    } else if (age > 17 && age < 50) {
+      adult += 1;
+    } else if (age >= 50) {
+      senior += 1;
+    }
+  });
+  const ageSeparator = { child, adult, senior };
+  return ageSeparator;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const entries = countEntrants(entrants);
+  const { prices } = data;
+  let fullPrice = 0;
+  const adultPrice = entries.adult * prices.adult;
+  const childPrice = entries.child * prices.child;
+  const seniorPrice = entries.senior * prices.senior;
+  fullPrice = adultPrice + childPrice + seniorPrice;
+  return fullPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
