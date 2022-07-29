da5971200ef5c1c2eb655258df737f925d224199
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2022-07-29 17:19:34 -0300

    R10: função getOldestFromFirstSpecies implementada

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,38 @@
 const data = require('../data/zoo_data');
 
+const searchEmployeer = (id) => {
+  const { employees } = data;
+  let employeer = {};
+  employees.forEach((employee) => {
+    if (employee.id === id) {
+      employeer = employee;
+      return employeer;
+    }
+  });
+  return employeer;
+};
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { species } = data;
+  let infoOldest;
+  const employee = searchEmployeer(id);
+  species.forEach((specie) => {
+    if (specie.id === employee.responsibleFor[0]) {
+      let oldest = specie.residents[0];
+      specie.residents.forEach((resident) => {
+        if (resident.age > oldest.age) {
+          oldest = resident;
+        }
+        infoOldest = [oldest.name, oldest.sex, oldest.age];
+        return infoOldest;
+      });
+      return infoOldest;
+    }
+  });
+  return infoOldest;
 }
+
+// console.log(searchEmployeer('c1f50212-35a6-4ecd-8223-f835538526c2'));
+console.log(getOldestFromFirstSpecies('c1f50212-35a6-4ecd-8223-f835538526c2'));
 
 module.exports = getOldestFromFirstSpecies;
