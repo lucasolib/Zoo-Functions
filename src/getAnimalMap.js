c966482496cd6060f718fd91a2c84a0fa374ab1f
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2022-08-02 11:24:04 -0300

    R14: função getAnimalMap implementada

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -14,37 +14,50 @@ const undefinedOptions = (locs, species) => {
   return fullAnimalsLoc;
 };
 
-const includeNamesTrue = (locs, species) => {
-  const fullAnimalsLoc = {};
-  locs.forEach((location) => {
-    const animalsLoc = [];
-    species.forEach((specie) => {
-      let animalsName = [];
-      if (location === specie.location) {
-        const { residents } = specie;
-        const key = specie.name;
-        const obj = {};
-        residents.forEach((resident) => {
-          animalsName = [...animalsName, resident.name];
-        });
-        obj[key] = animalsName;
-        animalsLoc.push(obj);
-      } fullAnimalsLoc[location] = animalsLoc;
+const getFilteredSex = (options, residents) => {
+  let animalsName = [];
+  if (options.sex === undefined) {
+    residents.forEach((resident) => {
+      animalsName = [...animalsName, resident.name];
     });
+  } else {
+    const filteredResidents = residents.filter((resident) => resident.sex === options.sex);
+    filteredResidents.forEach((resident) => {
+      animalsName = [...animalsName, resident.name];
+    });
+  }
+  return animalsName;
+};
+
+const getAnimalByLoc = (location, species) => {
+  const animal = [];
+  species.forEach((specie) => {
+    if (specie.location === location) {
+      animal.push(specie);
+    }
   });
-  return fullAnimalsLoc;
+  return animal;
 };
 
 function getAnimalMap(options) {
   const { species } = data;
   const locs = ['NE', 'NW', 'SE', 'SW'];
+  const fullAnimalsLoc = {};
   if (options === undefined || options.includeNames === undefined) {
     return undefinedOptions(locs, species);
-  } if (options.includeNames === true) {
-    return includeNamesTrue(locs, species);
-  }
+  } locs.forEach((location) => {
+    const animalsLoc = [];
+    const animals = getAnimalByLoc(location, species);
+    animals.forEach((specie) => {
+      const { residents } = specie;
+      const obj = {};
+      const filteredAnimals = getFilteredSex(options, residents);
+      const animalsName = (options.sorted === true) ? filteredAnimals.sort() : filteredAnimals;
+      obj[specie.name] = animalsName;
+      animalsLoc.push(obj);
+      fullAnimalsLoc[location] = animalsLoc;
+    });
+  }); return fullAnimalsLoc;
 }
-
-console.log(JSON.stringify(getAnimalMap({ includeNames: true, sorted: true }), null, '\t'));
 
 module.exports = getAnimalMap;
